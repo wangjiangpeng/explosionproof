d42ac7213ea88e1bcce50f151ecfb2a775da1b56
wangjp <675650218@qq.com>
2023-04-19 13:53:49 +0800

    optimize home layout

--- app/src/main/java/com/lanhu/explosion/view/HomeView.java
+++ app/src/main/java/com/lanhu/explosion/view/HomeView.java
@@ -1,5 +1,6 @@
 package com.lanhu.explosion.view;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.util.AttributeSet;
@@ -8,12 +9,13 @@ import android.view.LayoutInflater;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.lanhu.explosion.CommActivity;
 import com.lanhu.explosion.ExplosionActivity;
@@ -39,12 +41,18 @@ public class HomeView extends LinearLayout implements TaskCallback {
 
     TextView mMemTV;
     TextView mStatusTV;
-    GridView mGasGV;
+    RecyclerView mGasRV;
+    RecyclerView mSettingsRV;
     TextureView sv;
 
     GasInfo mInfo;
     Button mRecordBtn;
     TextView mRecordTV;
+
+    private SettingsItem[] mSetItems = {
+            new SettingsItem(R.drawable.comm_dw, R.string.explosion_comm, CommActivity.class),
+            new SettingsItem(R.drawable.collect_dw, R.string.explosion_collect, GasCollectActivity.class),
+            new SettingsItem(R.drawable.setting_dw, R.string.explosion_setting, SettingsActivity.class)};
 
     public HomeView(Context context) {
         super(context);
@@ -61,12 +69,14 @@ public class HomeView extends LinearLayout implements TaskCallback {
         init(context);
     }
 
+    @SuppressLint("WrongViewCast")
     private void init(Context context) {
         LayoutInflater.from(context).inflate(R.layout.home_view, this);
 
         mMemTV = findViewById(R.id.home_view_men);
         mStatusTV = findViewById(R.id.home_view_status);
-        mGasGV = findViewById(R.id.home_view_grid_gas);
+        mGasRV = findViewById(R.id.home_view_gas);
+        mSettingsRV = findViewById(R.id.home_view_settings);
         sv = findViewById(R.id.home_full_grid_gas_tv);
         mRecordBtn = findViewById(R.id.home_view_open_camera);
         mRecordTV = findViewById(R.id.home_view_record_text);
@@ -113,7 +123,7 @@ public class HomeView extends LinearLayout implements TaskCallback {
             @Override
             public void onClick(View v) {
                 CameraRecordTask task = TaskService.getInstance().getTask(CameraRecordTask.class);
-                if(!task.isRuned()){
+                if (!task.isRuned()) {
                     task.reExecute();
                     mRecordTV.setText(R.string.explosion_recording);
                 } else {
@@ -124,7 +134,14 @@ public class HomeView extends LinearLayout implements TaskCallback {
         });
 
         mInfo = GasInfo.sInfo;
-        mGasGV.setAdapter(new GasAdapter(mInfo));
+
+        GridLayoutManager gasManager = new GridLayoutManager(getContext(), 4, RecyclerView.VERTICAL, false);
+        mGasRV.setAdapter(new GasRecyclerAdapter(mInfo));
+        mGasRV.setLayoutManager(gasManager);
+
+        GridLayoutManager settingsManager = new GridLayoutManager(getContext(), 3, RecyclerView.VERTICAL, false);
+        mSettingsRV.setAdapter(new SettingsRecyclerAdapter());
+        mSettingsRV.setLayoutManager(settingsManager);
     }
 
     @Override
@@ -147,101 +164,157 @@ public class HomeView extends LinearLayout implements TaskCallback {
         } else if (task instanceof GasCollectTask) {
             if (result != null) {
                 mInfo = (GasInfo) result;
-                mGasGV.setAdapter(new GasAdapter(mInfo));
+                mGasRV.setAdapter(new GasRecyclerAdapter(mInfo));
             }
         }
     }
 
-    private class GasAdapter extends BaseAdapter {
+    private class GasVH extends RecyclerView.ViewHolder {
+
+        ImageView icon;
+        TextView name;
+        TextView value;
+        TextView status;
+        TextView data;
+
+        public GasVH(View itemView) {
+            super(itemView);
+
+            icon = itemView.findViewById(R.id.explosion_item_gas_icon);
+            name = itemView.findViewById(R.id.explosion_item_gas_name);
+            value = itemView.findViewById(R.id.explosion_item_gas_value);
+            status = itemView.findViewById(R.id.explosion_item_gas_status);
+            data = itemView.findViewById(R.id.explosion_item_gas_date);
+        }
+    }
+
+    private class GasRecyclerAdapter extends RecyclerView.Adapter<GasVH> {
 
         GasInfo info;
 
-        GasAdapter(GasInfo info) {
+        GasRecyclerAdapter(GasInfo info) {
             this.info = info;
         }
 
         @Override
-        public int getCount() {
-            return 4;
+        public GasVH onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new GasVH(LayoutInflater.from(parent.getContext()).inflate(R.layout.explosion_item_gas, parent, false));
         }
 
         @Override
-        public Object getItem(int position) {
-            return info;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.explosion_item_gas, parent, false);
-            }
-
-            ImageView icon = convertView.findViewById(R.id.explosion_item_gas_icon);
-            TextView name = convertView.findViewById(R.id.explosion_item_gas_name);
-            TextView value = convertView.findViewById(R.id.explosion_item_gas_value);
-            TextView status = convertView.findViewById(R.id.explosion_item_gas_status);
-            TextView data = convertView.findViewById(R.id.explosion_item_gas_date);
-
+        public void onBindViewHolder(GasVH holder, int position) {
             switch (position) {
                 case 0:
-                    icon.setImageResource(R.mipmap.oxygen);
-                    name.setText(R.string.explosion_hydrogen);
-                    value.setText(String.valueOf(info.O2));
+                    holder.icon.setImageResource(R.mipmap.oxygen);
+                    holder.name.setText(R.string.explosion_oxygen);
+                    holder.value.setText(String.valueOf(info.O2));
                     if (info.status_O2 == GasInfo.STATUS_OK) {
-                        status.setTextColor(getResources().getColor(R.color.green, null));
-                        status.setText(R.string.explosion_qualified);
+                        holder.status.setTextColor(getResources().getColor(R.color.green, null));
+                        holder.status.setText(R.string.explosion_qualified);
                     } else {
-                        status.setTextColor(getResources().getColor(R.color.red, null));
-                        status.setText(R.string.explosion_warn);
+                        holder.status.setTextColor(getResources().getColor(R.color.red, null));
+                        holder.status.setText(R.string.explosion_warn);
                     }
                     break;
                 case 1:
-                    icon.setImageResource(R.mipmap.co);
-                    name.setText(R.string.explosion_co);
-                    value.setText(String.valueOf(info.CO));
+                    holder.icon.setImageResource(R.mipmap.co);
+                    holder.name.setText(R.string.explosion_co);
+                    holder.value.setText(String.valueOf(info.CO));
                     if (info.status_CO == GasInfo.STATUS_OK) {
-                        status.setTextColor(getResources().getColor(R.color.green, null));
-                        status.setText(R.string.explosion_qualified);
+                        holder.status.setTextColor(getResources().getColor(R.color.green, null));
+                        holder.status.setText(R.string.explosion_qualified);
                     } else {
-                        status.setTextColor(getResources().getColor(R.color.red, null));
-                        status.setText(R.string.explosion_warn);
+                        holder.status.setTextColor(getResources().getColor(R.color.red, null));
+                        holder.status.setText(R.string.explosion_warn);
                     }
                     break;
                 case 2:
-                    icon.setImageResource(R.mipmap.burn_gas);
-                    name.setText(R.string.explosion_burn_gas);
-                    value.setText(String.valueOf(info.CH4));
+                    holder.icon.setImageResource(R.mipmap.burn_gas);
+                    holder.name.setText(R.string.explosion_burn_gas);
+                    holder.value.setText(String.valueOf(info.CH4));
                     if (info.status_CH4 == GasInfo.STATUS_OK) {
-                        status.setTextColor(getResources().getColor(R.color.green, null));
-                        status.setText(R.string.explosion_qualified);
+                        holder.status.setTextColor(getResources().getColor(R.color.green, null));
+                        holder.status.setText(R.string.explosion_qualified);
                     } else {
-                        status.setTextColor(getResources().getColor(R.color.red, null));
-                        status.setText(R.string.explosion_warn);
+                        holder.status.setTextColor(getResources().getColor(R.color.red, null));
+                        holder.status.setText(R.string.explosion_warn);
                     }
                     break;
                 case 3:
-                    icon.setImageResource(R.mipmap.hydrogen);
-                    name.setText(R.string.explosion_hydrogen);
-                    value.setText(String.valueOf(info.H2S));
+                    holder.icon.setImageResource(R.mipmap.hydrogen);
+                    holder.name.setText(R.string.explosion_hydrogen);
+                    holder.value.setText(String.valueOf(info.H2S));
                     if (info.status_H2S == GasInfo.STATUS_OK) {
-                        status.setTextColor(getResources().getColor(R.color.green, null));
-                        status.setText(R.string.explosion_qualified);
+                        holder.status.setTextColor(getResources().getColor(R.color.green, null));
+                        holder.status.setText(R.string.explosion_qualified);
                     } else {
-                        status.setTextColor(getResources().getColor(R.color.red, null));
-                        status.setText(R.string.explosion_warn);
+                        holder.status.setTextColor(getResources().getColor(R.color.red, null));
+                        holder.status.setText(R.string.explosion_warn);
                     }
                     break;
             }
 
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            data.setText(dateFormat.format(new Date(info.time)));
+            holder.data.setText(dateFormat.format(new Date(info.time)));
+        }
 
-            return convertView;
+        @Override
+        public int getItemCount() {
+            return 4;
+        }
+    }
+
+    private class SettingsVH extends RecyclerView.ViewHolder {
+
+        ImageView icon;
+        TextView name;
+        View layout;
+
+        public SettingsVH(View itemView) {
+            super(itemView);
+
+            icon = itemView.findViewById(R.id.home_settings_item_icon);
+            name = itemView.findViewById(R.id.home_settings_item_name);
+            layout = itemView.findViewById(R.id.home_settings_layout);
+        }
+    }
+
+    private class SettingsRecyclerAdapter extends RecyclerView.Adapter<SettingsVH> {
+
+        @Override
+        public SettingsVH onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new SettingsVH(LayoutInflater.from(parent.getContext()).inflate(R.layout.home_settings_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(HomeView.SettingsVH holder, int position) {
+            SettingsItem item = mSetItems[position];
+            holder.icon.setImageResource(item.iconId);
+            holder.name.setText(item.nameId);
+
+            holder.layout.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getContext().startActivity(new Intent(getContext(), item.jumpClass));
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mSetItems.length;
+        }
+    }
+
+    private class SettingsItem {
+        int iconId;
+        int nameId;
+        Class jumpClass;
+
+        SettingsItem(int iconId, int nameId, Class jumpClass) {
+            this.iconId = iconId;
+            this.nameId = nameId;
+            this.jumpClass = jumpClass;
         }
     }
 
